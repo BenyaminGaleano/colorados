30badd63746f24b110c1ad32d12b159957c6aa28
Benyamin Galeano <benyamin.galeano@galileo.edu>
2021-02-28 01:56:39 -0600

    links simbolicos

--- linkfiles.sh
+++ linkfiles.sh
@@ -12,6 +12,48 @@ ln -rs threads tests/threads/
 ln -rs threads userprog/
 ln -rs devices threads/
 
+# generando enlaces de userprog
+echo -e '\n\e[32mEnlaces para userprog\e[0m\n'
+ln -rs userprog userprog/
+
+# generando enlaces de filesys
+echo -e '\n\e[32mEnlaces para filesys\e[0m\n'
+ln -rs filesys userprog/
+ln -rs filesys filesys/
+
+# generando enlaces a las cabeceras para devices
+echo -e '\n\e[32mEnlaces para devices\e[0m\n'
+ln -rs devices devices/
+ln -rs devices filesys/
+
+# generando enlaces a las cabeceras del sistema
+echo -e '\n\e[32mEnlaces para lib\e[0m\n'
+cd ./lib/
+sudo ln -rs debug.h $inclDir
+sudo ln -rs random.h $inclDir
+sudo ln -rs round.h $inclDir
+cd ..
+
+# generando enlaces a cabeceras necesarios para kernel o user
+echo -e '\n\e[32mEnlaces para lib/kernel\e[0m\n'
+cd ./lib/kernel/
+sudo ln -rs list.h $inclDir
+sudo ln -rs console.h $inclDir
+sudo ln -rs bitmap.h $inclDir
+cd ../..
+
+
+echo -e '\n\e[32mEnlaces para lib/user/\e[0m\n'
+cd ./lib/user/
+
+cd ../..
+
+# generando enlaces a las cabeceras para test/threads
+echo -e '\n\e[32mEnlaces para devices\e[0m\n'
+echo ''
+ln -rs tests tests/threads
+
+
 
 # generando enlaces a las cabeceras para devices
 echo -e '\n\e[32mEnlaces para devices\e[0m\n'
