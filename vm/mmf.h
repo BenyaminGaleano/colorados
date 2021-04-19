a6f7379a2e3a1438cebe2eeb6832310e9cd63ea5
Benyamin Galeano <benyamin.galeano@galileo.edu>
2021-04-18 20:37:43 -0600

    Iniciando memory mapped files

---
+++ vm/mmf.h
@@ -0,0 +1,34 @@
+#ifndef __MMF_H__
+#define __MMF_H__
+#include <list.h>
+#include <stdio.h>
+#include <debug.h>
+#include "threads/pte.h"
+#include "threads/vaddr.h"
+
+struct mfile
+{
+    struct list_elem elem;
+    void * start;
+    void * end; // it's the last address mapped in file (included)
+    int fd;
+};
+
+static inline off_t
+mf_offset(struct mfile *s, void *fault_addr)
+{
+  ASSERT(pg_ofs(s->start) == 0);
+  ASSERT(fault_addr >= s->start && fault_addr <= s->end);
+  return ((uint64_t) pg_round_down(fault_addr) - (uint64_t) s->start);
+}
+
+static inline unsigned
+mf_read_bytes(struct mfile *s, void *fault_addr)
+{ 
+  ASSERT(pg_ofs(s->start) == 0);
+  ASSERT(fault_addr >= s->start && fault_addr < s->end);
+  return fault_addr >= pg_round_down(s->end) && fault_addr < pg_round_up(s->end) ? 
+    (uint64_t) s->end - (uint64_t) pg_round_down(s->end) + 1: PGSIZE;
+}
+
+#endif
